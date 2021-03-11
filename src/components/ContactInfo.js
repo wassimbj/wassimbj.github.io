b5775f88422de9982235e6fd9d942c6834cb10a7
wassimbj <wassimbenjdida@gmail.com>
2021-03-11 21:06:17 +0100

    new design

--- src/components/ContactInfo.js
+++ src/components/ContactInfo.js
@@ -4,14 +4,14 @@ export default function ContactInfo() {
   return (
     <React.Fragment>
       <div>
-        <h3 className="text-xl text-gray-600 mb-3"> Start a conversation </h3>
+        <h3 className="text-lg text-gray-600 mb-3"> Start a conversation </h3>
         <span className="p-2 px-4 rounded-full border border-gray-800 hover:border-gray-700 inline-block transition"> wassimjdida.dev@gmail.com </span>
       </div>
 
       {/* Social links */}
       <div className="mt-10">
-        <h3 className="text-xl text-gray-600 mb-3"> Find me on </h3>
-        <ul className="flex items-center flex-wrap">
+        <h3 className="text-lg text-gray-600 mb-3"> Find me on </h3>
+        <ul className="flex items-center justify-center flex-wrap">
           <li>
             <a
               href="https://linkedin.com/in/wassimdev"
@@ -31,17 +31,6 @@ export default function ContactInfo() {
               className="linkAboutMe transition"
             >
               Github
-            </a>
-          </li>
-          <li className="mx-3 text-gray-800"></li>
-          <li>
-            <a
-              href="https://codepen.io/wassimbj"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="linkAboutMe transition"
-            >
-              Codepen
             </a>
           </li>
           <li className="mx-3 text-gray-800"></li>
--- src/sections/About.js
+++ src/sections/About.js
@@ -1,46 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component } from "react"
 
 export default class About extends Component {
+  render() {
+    const myAge = 19
 
-    render() {
-        
-        const myAge = 19;
-
-        return (
-            <section className='about' id='about'>
-                <div className='container mx-auto px-3'>
-                    <div className='grid md:grid-cols-2 gap-12'>
-                        <div className='aboutMe'>
-                            <h3 className='tinyTitle mb-3'> About me ? </h3>
-                            <p>
-                                Hey there, as you already know my name is wassim. im {myAge}-years-old software developer.
-                                im so passionate about software engineering. and i love to drink coffee ☕
-                            </p>
-                        </div>
-
-                        <div className='findMe'>
-                            <h3 className='tinyTitle mb-3'> I can build anything with </h3>
-                            <span className="skill cursor-hover:bg-black hover:text-white"> Nodejs </span>
-                            <span className="skill cursor-hover:bg-black hover:text-white"> Express.js </span>
-                            <span className="skill cursor-hover:bg-black hover:text-white"> Javascript </span>
-                            <span className="skill cursor-hover:bg-black hover:text-white"> Go </span>
-                            <span className="skill cursor-hover:bg-black hover:text-white"> APIs </span>
-                            <span className="skill cursor-hover:bg-black hover:text-white"> React.js </span>
-                            <span className="skill cursor-hover:bg-black hover:text-white"> MySQL </span>
-                            <span className="skill cursor-hover:bg-black hover:text-white"> GraphQL </span>
-                            <span className="skill cursor-hover:bg-black hover:text-white"> Docker </span>
-                            <span className="skill cursor-hover:bg-black hover:text-white"> MongoDB </span>
-                            <span className="skill cursor-hover:bg-black hover:text-white"> PostgreSql </span>
-                            <span className="skill cursor-hover:bg-black hover:text-white"> Redis </span>
-                            <span className="skill cursor-hover:bg-black hover:text-white"> Git </span>
-                            <span className="skill cursor-hover:bg-black hover:text-white"> Ajax </span>
-                            <span className="skill cursor-hover:bg-black hover:text-white"> JQuery </span>
-                            <span className="skill cursor-hover:bg-black hover:text-white"> Css </span>
-                            <span className="skill cursor-hover:bg-black hover:text-white"> HTML </span>
-                        </div>
-                    </div>
-                </div>
-            </section>
-        )
-    }
+    return (
+      <section className="about" id="about">
+        <div className="mx-auto px-2 max-w-3xl text-lg">
+          <div className="text-center">
+            <div className="text-gray-400">
+              <p>
+                Hey there 😄, my name is wassim. im {myAge}
+                -years-old software developer. im so passionate about software
+                engineering, im now working mostly with Javascript (node, react),
+                and im in the process of learning new things like Go, Elixir and Distributed systems.🤩
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    )
+  }
 }
