d7f064e8e4d40e5486a703886021d4cff9c230fa
Wassim <wassimbenjdida@gmail.com>
2021-05-24 01:46:14 +0100

    Update About.js

--- src/sections/About.js
+++ src/sections/About.js
@@ -2,18 +2,14 @@ import React, { Component } from "react"
 
 export default class About extends Component {
   render() {
-    const myAge = 19
-
     return (
       <section className="about" id="about">
         <div className="mx-auto px-2 max-w-3xl text-lg">
           <div className="text-center">
             <div className="text-gray-400">
               <p>
-                Hey there, my name is wassim. im {myAge}
-                -years-old software developer. im so passionate about software
-                engineering, im now working mostly with Javascript (node, react),
-                and im in the process of learning new things like Go, Elixir and Distributed systems.
+                Hey there, my name is Wassim im 20-years-old software developer from Tunisia,
+                i enjoy building stuff, learning new things, and drinking coffee.
               </p>
             </div>
           </div>
